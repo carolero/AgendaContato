9cb51de6f8bdf3bdc509630ed3c59a6d9328ccf5
Carol <carolina.oliveira@zup.com.br>
2019-05-30 00:14:00 -0300

    Melhorando procura e remoção de contato

--- src/br/com/zup/exercicio/tres/AgendaContatos.java
+++ src/br/com/zup/exercicio/tres/AgendaContatos.java
@@ -11,20 +11,22 @@ public class AgendaContatos {
 		// Iniciando o Scanner
 		Scanner scan = new Scanner(System.in);
 		
-		// Opções
-		System.out.println("Digite:");
-		System.out.println("1 para criar um contato");
-		System.out.println("2 para buscar um telefone pelo nome do contato");
-		System.out.println("3 para apagar um contato");
-		int escolha = scan.nextInt();
-		
 		// Criando lista
-		List<String> contatos = new ArrayList<String>();
+		List<Contato> contatos = new ArrayList<Contato>();
 		
-		int controleExterno = 1;
+		int contatosTotais = 0;
 		
-		// Validando opções
-		while (controleExterno == 1) {
+		int controlePrincipal = 1;
+		
+		while (controlePrincipal == 1) {
+			// Opções
+			System.out.println("Digite:");
+			System.out.println("1 para criar um contato");
+			System.out.println("2 para buscar um telefone pelo nome do contato");
+			System.out.println("3 para apagar um contato");
+			int escolha = scan.nextInt();
+			
+			// Validando opções
 			if (escolha == 1) {
 				Scanner entradaContato = new Scanner(System.in);
 				
@@ -32,21 +34,28 @@ public class AgendaContatos {
 				int controle = 1;
 				
 				while (controle == 1) {
+					// CRIANDO CONTATO
 					System.out.println("Nome do contato: ");
 					String nome = entradaContato.nextLine();
 					
 					System.out.println("Qual o numero do contato?");
 					String telefone = entradaContato.nextLine();
 					
-					contatos.add(nome);
-					contatos.add(telefone);
-				
-					for (String string : contatos) {
-						System.out.println(string);
-					}
+					Contato novoContato = new Contato(nome, telefone);	
+					
+					contatos.add(novoContato);
+										
+					contatosTotais++;
+					
+					System.out.println("Você tem " + contatosTotais + " contato/s salvo/s em sua lista.");
 					
 					System.out.println("Para adicionar mais um contato digite 1, para encerrar digite 0");
 					controle = scan.nextInt();
+					
+					// Teste para confirmar a criação de contato
+					for (Contato contato : contatos) {
+						System.out.println(contato);
+					}
 					
 				}
 				
@@ -57,11 +66,10 @@ public class AgendaContatos {
 				System.out.println("Qual contato quer procurar?");
 				String procura = entradaProcura.nextLine();
 				
-				if (contatos.contains(procura)) {
-					System.out.println(contatos.indexOf(procura));
-				} else {
-					System.out.println("Esse contato não existe.");
-				}
+				int indice = procurarIndice(procura, contatos);
+
+				String resultado = contatos.get(indice).getTelefone();
+				System.out.println(resultado);
 				
 				
 			} else if (escolha == 3) {
@@ -72,24 +80,42 @@ public class AgendaContatos {
 				System.out.println("Qual contato quer apagar?");
 				remover = entradaRemovendo.nextLine();
 				
-				if (contatos.contains(remover)) {
-					contatos.remove(remover);
-					System.out.println("Contato removido.");
-
-				} else {
-					System.out.println("Esse contato não existe.");
-				}			
+				int indice = procurarIndice(remover, contatos);
+				
+				contatos.remove(indice);
+				System.out.println("Contato apagado com sucesso");
+				
+				contatosTotais--;
+				
+				System.out.println("Agora sua lista tem " + contatosTotais + " contato/s.");				
+				
+				// Teste para confirmar a remoção
+//				for (Contato contato : contatos) {
+//					System.out.println(contato);
+//				}
 				
 			} else {
 				System.out.println("Opção inválida.");
 			}
+			
+			System.out.println("Se deseja retornar ao menu de opções inicial digite 1 ou 0 para sair");
+			Scanner novaOpcao = new Scanner(System.in);
+			controlePrincipal = novaOpcao.nextInt();
+			
 		}
 		
-		
 	}
-		
 	
-	public void opcao() {
+	public static int procurarIndice(String nome, List<Contato> contatos) {
+		int posicao = 0;
+		
+		for (Contato object : contatos) {
+			if(object.getNome().equalsIgnoreCase(nome)) {
+				posicao = contatos.indexOf(object);
+			}
+		}
+				
+		return posicao;		
 		
 	}
 
